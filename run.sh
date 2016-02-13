3b9ebdfb9cc8258fd71b647c91a7e02f84265a38
Matthew Stratton <development@mmstratton.com>
2016-02-13 15:46:09 -0500

    fixing some errors with run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-sudo yun install -y python-setuptools git python-dev libxml2-dev libxslt-dev
+sudo dnf install -y python-setuptools git python-devel libxml2-devel libxslt-devel redhat-rpm-config
 sudo easy_install pip
 sudo pip install -r requirements.txt
 
-ansible-playbook setup.yml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules --extra-vars "@config.json"
+ansible-playbook setup.yaml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules --extra-vars "@config.json"
