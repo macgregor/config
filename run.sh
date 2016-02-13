b071145896c6c03f2394959148bf7cb1387130a5
Matthew Stratton <development@mmstratton.com>
2016-02-13 18:39:01 -0500

    prettying up run.sh output and adding logging

--- run.sh
+++ run.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
-sudo dnf install -y python-setuptools git python-devel python-dnf libxml2-devel libxslt-devel redhat-rpm-config libselinux-python
-sudo easy_install pip
-sudo pip install -r requirements.txt
+echo "Installing required packages"
+echo "========== Starting Setup ==========" >> ./setup.log
+echo "$(date)" >> ./setup.log
 
-ansible-playbook setup.yaml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules --extra-vars "@config.json"
+sudo dnf install -y python-setuptools git python-devel python-dnf libxml2-devel libxslt-devel redhat-rpm-config libselinux-python &>> ./setup.log
+
+echo "Installing pip"
+sudo easy_install pip &>> ./setup.log
+
+echo "Installing Ansible"
+sudo pip install -r requirements.txt &>> ./setup.log
+
+ansible-playbook setup.yaml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules --extra-vars "@config.json" 2>&1 | tee -a ./setup.log
+
+echo "========== Finished Setup ==========" >> ./setup.log
+echo "Set up complete, see ./setup.log for details"
