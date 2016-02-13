4fdfb5cebcd9eb69c03bb241805d80f3289c88d6
Matthew Stratton <development@mmstratton.com>
2016-02-13 16:44:15 -0500

    missing dnf dependency for selinux

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
-sudo dnf install -y python-setuptools git python-devel python-dnf libxml2-devel libxslt-devel redhat-rpm-config
+sudo dnf install -y python-setuptools git python-devel python-dnf libxml2-devel libxslt-devel redhat-rpm-config libselinux-python
 sudo easy_install pip
 sudo pip install -r requirements.txt
 
-ansible-playbook -vvv setup.yaml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules --extra-vars "@config.json"
+ansible-playbook setup.yaml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules --extra-vars "@config.json"
