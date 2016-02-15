f670d274dc101ad12d0fec294cb4d15cfd4630a8
Matthew Stratton <development@mmstratton.com>
2016-02-15 08:47:17 -0500

    finally got powerline working

--- run.sh
+++ run.sh
@@ -10,6 +10,7 @@ sudo easy_install pip &>> ./setup.log
 
 echo "Installing Ansible"
 sudo pip install -r requirements.txt &>> ./setup.log
+sudo ansible-galaxy install yaegashi.blockinfile &>> ./setup.log
 
 ansible-playbook setup.yaml -i HOSTS --ask-sudo-pass  --module-path ./ansible_modules --extra-vars "@config.json" 2>&1 | tee -a ./setup.log
 
