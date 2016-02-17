bb2933e9435a053fb2cc9b11d7c86c9ff8294135
Matthew Stratton <development@mmstratton.com>
2016-02-17 13:27:33 -0500

    making bash histroy global across all terminals

--- roles/common/files/bashrc.d/history.bash
+++ roles/common/files/bashrc.d/history.bash
@@ -1,5 +1,9 @@
 shopt -s histappend                # enable history appending instead of overwriting
 HISTSIZE=100000                    # remember up to 100.000 entries
 HISTFILESIZE=$HISTSIZE             # store as many entries in file as in memory
-HISTFILE=$HOME/.history            # share history file with zsh
 HISTCONTROL=ignoredups:ignorespace # ignore lines starting with a whitespace, as well as duplicates
+HISTTIMEFORMAT="%m/%d/%y %T "      # add timestamp to history lines
+HISTIGNORE='ls -l:pwd:date:'       # ignore these commands
+
+# After each command, append to the history file and reread it
+export PROMPT_COMMAND="${PROMPT_COMMAND:+$PROMPT_COMMAND$'\n'}history -a; history -c; history -r"  
