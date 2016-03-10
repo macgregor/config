d0e1c57e331b2e242b9864dedd57dc71f537e418
Matthew Stratton <mstratto@redhat.com.com>
2016-03-09 23:31:46 -0500

    moving some lininfile commands to bashrc source files for simplicity turning bashrc.d scripts into templates

--- roles/dotfiles/files/bashrc.d/alias.bash
+++ roles/dotfiles/files/bashrc.d/alias.bash
@@ -1,3 +1,10 @@
+export GRADLE_HOME=/usr/share/gradle-2.11
+export JAVA_HOME=/usr/lib/jvm/java
+export DROPBOX_ENCFS_ENCRYPTED_DIR={{dropbox_encfs_encrypted_dir}} 
+export DROPBOX_ENCFS_UNENCRYPTED_DIR={{dropbox_encfs_unencrypted_dir}}
+
+export PATH=$GRADLE_HOME/bin:$PATH
+
 # trick to make aliases available when using sudo
 alias sudo='sudo '
 
@@ -7,3 +14,6 @@ alias ci='vim'
 alias build_project="mvn clean install -DskipTests"
 alias test_project="mvn test -fae"
 alias bat='pygmentize -g'
+alias eclipse='nohup /usr/share/eclipse/eclipse &>/dev/null &'
+alias unmount_dropbox_encrypted='fusermount -u $DROPBOX_ENCFS_UNENCRYPTED_DIR'
+alias mount_dropbox_encrypted='encfs --idle=15 $DROPBOX_ENCFS_ENCRYPTED_DIR $DROPBOX_ENCFS_UNENCRYPTED_DIR'
--- roles/dotfiles/files/bashrc.d/functions.bash
+++ roles/dotfiles/files/bashrc.d/functions.bash
@@ -7,22 +7,6 @@ format_xml(){
     cat $1 | xmllint --format --output $1 -
 }
 
-dock(){
-    /usr/bin/xrandr --output eDP1 --auto --left-of DP2-2
-    /usr/bin/xrandr --output DP2-1 --right-of DP2-2
-    /usr/bin/xrandr --output DP2-2 --primary
-}
-
-dock_fix(){
-    /usr/bin/xrandr --output DP2-1 --off && /usr/bin/xrandr --output DP2-2 --off
-    /usr/bin/xrandr --output DP2-1 --auto && /usr/bin/xrandr --output DP2-2 --auto
-    dock
-}
-
-undock(){
-    /usr/bin/xrandr --output eDP1 --auto
-}
-
 # expand all tabs to 4 spaces
 no_tabs(){
     find . -name $1 ! -type d -exec bash -c 'expand -t 4 "$0" > /tmp/e && mv /tmp/e "$0"' {} \;
