55f00aa2c7dff38c0772dfc4557c1a94fff57924
Matthew Stratton <mstratto@redhat.com.com>
2016-03-09 23:57:21 -0500

    updating encfs setup to be boxcryptor compatible

--- roles/common/files/scripts/encfs_setup.sh
+++ roles/common/files/scripts/encfs_setup.sh
@@ -7,7 +7,34 @@ set pass [lindex $argv 2]
 spawn encfs $encrypted_dir $unencrypted_dir
 
 expect "?> "
-send "p\r"
+send "x\r"
+
+expect "Enter the number corresponding to your choice: "
+send "1\r"
+
+expect "Selected key size: "
+send "256\r"
+
+expect "filesystem block size: "
+send "1024\r"
+
+expect "Enter the number corresponding to your choice: "
+send "3\r"
+
+expect "Any response that does not begin with 'n' will mean Yes: "
+send "no\r"
+
+expect "Any response that does not begin with 'n' will mean Yes: "
+send "no\r"
+
+expect "Any response that does not begin with 'n' will mean Yes: "
+send "no\r"
+
+expect "Select a number of bytes, from 0 (no random bytes) to 8: "
+send "0\r"
+
+expect "Any response that does not begin with 'n' will mean Yes: "
+send "yes\r"
 
 expect "New Encfs Password: "
 send "$pass\r"
