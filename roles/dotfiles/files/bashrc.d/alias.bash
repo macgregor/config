3f3d8bca681087366dd3ba2516db9982447185ba
Matthew Stratton <development@mmstratton.com>
2016-03-11 14:53:46 -0500

    adding eclipse_install_dir variable, which lead to the discovery of some ansible behavior I wasnt aware of...
    So 1) ansible includes role/vars/main.yaml for all roles, even when that
    role is conditionally included. 2) variables inside role/vars/main.yaml
    are not namespaced. The last instance of a given variable wins out. Both
    roles/beastop/vars/main.yaml and roles/lenovo/vars/main.yaml defined
    eclipse_install_dir, but the path was being set to beastop's variable
    even on the lenovo laptop.
    
    Refactoring setup.yaml to dynamically include a role's variables based
    on the same condition I use to include the machine role itself.

--- roles/dotfiles/files/bashrc.d/alias.bash
+++ roles/dotfiles/files/bashrc.d/alias.bash
@@ -2,6 +2,7 @@ export GRADLE_HOME=/usr/share/gradle-2.11
 export JAVA_HOME=/usr/lib/jvm/java
 export DROPBOX_ENCFS_ENCRYPTED_DIR={{dropbox_encfs_encrypted_dir}} 
 export DROPBOX_ENCFS_UNENCRYPTED_DIR={{dropbox_encfs_unencrypted_dir}}
+export ECLIPSE_HOME={{eclipse_install_dir}}/eclipse
 
 export PATH=$GRADLE_HOME/bin:$PATH
 
@@ -14,6 +15,6 @@ alias ci='vim'
 alias build_project="mvn clean install -DskipTests"
 alias test_project="mvn test -fae"
 alias bat='pygmentize -g'
-alias eclipse='nohup /usr/share/eclipse/eclipse &>/dev/null &'
+alias eclipse='nohup $ECLIPSE_HOME/eclipse &>/dev/null &'
 alias unmount_dropbox_encrypted='fusermount -u $DROPBOX_ENCFS_UNENCRYPTED_DIR'
 alias mount_dropbox_encrypted='encfs --idle=15 $DROPBOX_ENCFS_ENCRYPTED_DIR $DROPBOX_ENCFS_UNENCRYPTED_DIR'
